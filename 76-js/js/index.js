45b276973b255b3eddff22efe4c83420d8aa9b41
devsergx <devsergx@gmail.com>
2026-04-18 11:07:01 +0500

    Add files via upload

---
+++ 76-js/js/index.js
@@ -0,0 +1,74 @@
+"use strict";
+
+// Массив с ключами
+const todoKeys = {
+  id: "id",
+  text: "text",
+  is_completed: "is_completed",
+};
+
+// Массив для задач
+const todos = [];
+
+// Сообщение об ошибке
+const errTodoNotFound = (todoId) => `Todo with ID ${todoId} not found`;
+
+// ID задачи
+const getNewTodoId = (todos) =>
+  todos.reduce((maxId, todo) => Math.max(maxId, todo[todoKeys.id]), 0) + 1;
+
+// Добавление
+const createTodo = (todos, text) => {
+  const newTodo = {
+    [todoKeys.id]: getNewTodoId(todos),
+    [todoKeys.text]: text,
+    [todoKeys.is_completed]: false,
+  };
+  todos.push(newTodo);
+  return newTodo;
+};
+
+// Выполнение
+const completeTodoById = (todos, todoId) => {
+  const todo = todos.find((todo) => todo[todoKeys.id] === todoId);
+  if (!todo) {
+    console.error(errTodoNotFound(todoId));
+    return null;
+  }
+  todo[todoKeys.is_completed] = !todo[todoKeys.is_completed];
+  return todo;
+};
+
+// Удаление
+const deleteTodoById = (todos, todoId) => {
+  const todoIndex = todos.findIndex((todo) => todo[todoKeys.id] === todoId);
+  if (todoIndex === -1) {
+    console.error(errTodoNotFound(todoId));
+    return todos;
+  }
+  todos.splice(todoIndex, 1);
+  return todos;
+};
+
+// При помощи метода querySelector получаем элементы .form, .input и .todos
+const form = document.querySelector(".form");
+const input = document.querySelector(".input");
+const tasks = document.querySelector(".tasks");
+
+// Создаем функцию createTodoElement(text), которая будет создавать todo в виде разметки
+function createTodoElement(text) {
+  const task = document.createElement("li");
+  task.classList.add("task");
+  task.innerHTML = `<div class="todo-text">${text}</div>
+                      <div class="todo-actions">
+                        <button class="button-complete button">&#10004;</button>
+                        <button class="button-delete button">&#10006;</button>
+                    </div>`;
+  tasks.append(task);
+}
+
+// Создаем функцию handleCreateTodo(todos, text), которая будет вызывать createTodo и createTodoElement
+function handleCreateTodo(todos, text) {
+  createTodo(todos);
+  createTodoElement(text);
+}
